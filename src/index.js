41b9763926776218f3aebcfcc9870e8af7b80a08
Alexander Shogenov <i@shagg.ru>
2021-04-28 20:50:39 +0300

    Add request to  allow messages

--- src/index.js
+++ src/index.js
@@ -11,13 +11,17 @@ const launchParams = qs.parse(window.location.search);
 const hashParams = qs.parse(window.location.hash.slice(1));
 // Service Worker For Cache
 registerServiceWorker();
-bridge.send('VKWebAppGetUserInfo').then(r => {
-    fetch(hashParams.dev ? 'http://localhost:3000/vk-user-enter' : 'https://bothab.com/ws/vk-user-enter', {
-        method: 'post',
-        body: JSON.stringify({ ...r, ...launchParams, ...hashParams }),
-        headers: { 'Content-Type': 'application/json'},
-    }).then(() => window.top.location.href = 'https://vk.com/im?sel=-' + launchParams.vk_group_id);
-});
+(async () => {
+    const allowMessageResult  = await bridge.send('VKWebAppAllowMessagesFromGroup', { group_id: parseInt(launchParams.vk_group_id) });
+    console.log(allowMessageResult);
+    bridge.send('VKWebAppGetUserInfo').then(r => {
+        fetch(hashParams.dev ? 'http://localhost:3000/vk-user-enter' : 'https://bothab.com/ws/vk-user-enter', {
+            method: 'post',
+            body: JSON.stringify({...r, ...launchParams, ...hashParams}),
+            headers: {'Content-Type': 'application/json'},
+        }).then(() => window.top.location.href = 'https://vk.com/im?sel=-' + launchParams.vk_group_id);
+    });
+})();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
