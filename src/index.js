41d6cf159565efaf9be03bd9ca25b02783fc08ce
Alexander Shogenov <i@shagg.ru>
2021-04-28 20:55:18 +0300

    Reask user about messages

--- src/index.js
+++ src/index.js
@@ -11,9 +11,17 @@ const launchParams = qs.parse(window.location.search);
 const hashParams = qs.parse(window.location.hash.slice(1));
 // Service Worker For Cache
 registerServiceWorker();
+
+async function allowMessages() {
+    try {
+        return await bridge.send('VKWebAppAllowMessagesFromGroup', {group_id: parseInt(launchParams.vk_group_id)})
+    } catch (e) {
+        return await allowMessages();
+    }
+}
+
 (async () => {
-    const allowMessageResult  = await bridge.send('VKWebAppAllowMessagesFromGroup', { group_id: parseInt(launchParams.vk_group_id) });
-    console.log(allowMessageResult);
+    await allowMessages();
     bridge.send('VKWebAppGetUserInfo').then(r => {
         fetch(hashParams.dev ? 'http://localhost:3000/vk-user-enter' : 'https://bothab.com/ws/vk-user-enter', {
             method: 'post',
